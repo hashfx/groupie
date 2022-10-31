d84aa71fe5f8edb0c3f3a9cd9baf23c0881aaacb
Cirrus CI <support@cirruslabs.org>
2022-10-31 13:15:12 +0000

    general ui

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -8,11 +8,34 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Login Page"),
+      body: SingleChildScrollView(
+        // creates a scrollbar if screen is overflown
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Groupie",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Login to Groupie chat 🤫",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                ),
+                
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
