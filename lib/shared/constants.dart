f5de382cda951a5090975dd5dd97015e0e8a16fe
Cirrus CI <support@cirruslabs.org>
2022-10-31 12:28:32 +0000

    homepage loginpage constants

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:381196456830:web:05e99a4080f3259a87592f";
+  static String apiKey = "AIzaSyCwDj3JP_sn-I9Bo1n1OxVnI4UkAt8Wogk";
+  static String messagingSenderId = "381196456830";
+  static String projectId = "groupie-70a49";
+}
