85243414fcc2d593c9efe4ea7f166a805c0dbaef
Cirrus CI <support@cirruslabs.org>
2022-11-01 10:36:15 +0000

    add primaryColor

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static String appId = "1:381196456830:web:05e99a4080f3259a87592f";
   static String apiKey = "AIzaSyCwDj3JP_sn-I9Bo1n1OxVnI4UkAt8Wogk";
   static String messagingSenderId = "381196456830";
   static String projectId = "groupie-70a49";
+  final primaryColor = const Color(0xFFee7b64);
 }
