2e7c1e5d08704eb2893c8e7023e406749363325a
Cirrus CI <support@cirruslabs.org>
2022-11-01 10:34:18 +0000

    primary color

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Constants().primaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
       debugShowCheckedModeBanner: false,
       home: _isSignedIn
           ? const HomePage()
