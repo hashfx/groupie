b932affaa42455be67918b3fc6fdefe64e2d633c
Cirrus CI <support@cirruslabs.org>
2022-11-02 14:06:41 +0000

    login(): with db snapshot

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:groupie/helper/helper_function.dart';
 import 'package:groupie/pages/auth/register_page.dart';
+import 'package:groupie/pages/home_page.dart';
+import 'package:groupie/service/auth_service.dart';
+import 'package:groupie/service/database_service.dart';
 import 'package:groupie/widgets/widgets.dart';
 
 class LoginPage extends StatefulWidget {
@@ -14,126 +20,167 @@ class _LoginPageState extends State<LoginPage> {
   final formkey = GlobalKey<FormState>();
   String email = "";
   String password = "";
+  bool _isLoading = false;
+  AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: SingleChildScrollView(
-        // creates a scrollbar if screen is overflown
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
-          child: Form(
-            key: formkey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  "Groupie",
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "Login to Groupie chat 🤫",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(height: 10),
-                Image.asset("assets/login.png"),
-                SizedBox(height: 10),
-                TextFormField(
-                  decoration: textInputDecoration.copyWith(
-                      labelText: "Email",
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Theme.of(context).primaryColor,
-                      )),
-                  onChanged: (val) {
-                    setState(() {
-                      email = val;
-                    });
-                  },
-
-                  // validate email
-                  validator: (val) {
-                    return RegExp(
-                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                            .hasMatch(val!)
-                        ? null
-                        : "Enter a Valid Mail";
-                  },
-                ),
-                const SizedBox(height: 15),
-                TextFormField(
-                    obscureText: true, // doesn't display to user
-                    decoration: textInputDecoration.copyWith(
-                        labelText: "Password",
-                        prefixIcon: Icon(
-                          Icons.lock,
-                          color: Theme.of(context).primaryColor,
-                        )),
-                    validator: (val) {
-                      if (val!.length < 6) {
-                        return "Password must be atleast 6 characters!";
-                      } else {
-                        return null;
-                      }
-                    },
-                    onChanged: (val) {
-                      setState(() {
-                        password = val;
-                      });
-                    }),
-                SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        )),
-                    child: const Text(
-                      "Sign In",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
+            ))
+          : SingleChildScrollView(
+              // creates a scrollbar if screen is overflown
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+                child: Form(
+                  key: formkey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Groupie",
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    onPressed: () {
-                      login();
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text.rich(
-                  TextSpan(
-                    text: "Create an Account now",
-                    style: const TextStyle(color: Colors.black, fontSize: 14),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: "Register Here",
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Login to Groupie chat 🤫",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(height: 10),
+                      Image.asset("assets/login.png"),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                            labelText: "Email",
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Theme.of(context).primaryColor,
+                            )),
+                        onChanged: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+
+                        // validate email
+                        validator: (val) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(val!)
+                              ? null
+                              : "Enter a Valid Mail";
+                        },
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                          obscureText: true, // doesn't display to user
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Password",
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Theme.of(context).primaryColor,
+                              )),
+                          validator: (val) {
+                            if (val!.length < 6) {
+                              return "Password must be atleast 6 characters!";
+                            } else {
+                              return null;
+                            }
+                          },
+                          onChanged: (val) {
+                            setState(() {
+                              password = val;
+                            });
+                          }),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              elevation: 2,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              )),
+                          child: const Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          onPressed: () {
+                            login();
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text.rich(
+                        TextSpan(
+                          text: "Create an Account now",
                           style: const TextStyle(
-                              color: Colors.black,
-                              decoration: TextDecoration.underline),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              nextScreen(context, const RegisterPage());
-                            }),
+                              color: Colors.black, fontSize: 14),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: "Register Here",
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    decoration: TextDecoration.underline),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    nextScreen(context, const RegisterPage());
+                                  }),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 
-  login() {}
+  login() async {
+    if (formkey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      await authService
+          .loginWithEmailandPassword(email, password)
+          .then((value) async {
+        if (value == true) {
+          // user press "LOGIN" button; search userData inside DataBase and get snapshot
+          QuerySnapshot snapshot = await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid).getUserData(email);
+          // save value to shared preferences
+          await HelperFunctions.saveUserLoggedInStatus(true);
+          await HelperFunctions.saveUserEmailSF(email);
+          await HelperFunctions.saveUserNameSF(
+            snapshot.docs[0]['fullname']
+          );
+
+
+          nextScreenReplace(context, const HomePage());
+        } else {
+          // error handling
+          showSnackBar(context, Colors.red, value);
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      });
+    }
+  }
 }
