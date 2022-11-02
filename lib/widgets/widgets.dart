470c0b32c8d599ac827e2e2617429f960f3efdc8
Cirrus CI <support@cirruslabs.org>
2022-11-02 10:44:47 +0000

    screen replace + snack bar for error function

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -21,6 +21,23 @@ void nextScreen(context, page) {
   Navigator.push(context, MaterialPageRoute(builder: (context) => page));
 }
 
-void nextScreenReplacement(context, page) {
-  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => page));
+void nextScreenReplace(context, page) {
+  Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => page));
+}
+
+void showSnackBar(context, color, message) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      message,
+      style: const TextStyle(fontSize: 14),
+    ),
+    backgroundColor: color,
+    duration: Duration(seconds: 2),
+    action: SnackBarAction(
+      label: "OK",
+      onPressed: () {},
+      textColor: Colors.white,
+    ),
+  ));
 }
