6c9092afb33b21cd39f93911f3be0e1e4e9be272
Cirrus CI <support@cirruslabs.org>
2022-11-03 12:45:45 +0000

    variables init

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
@@ -8,6 +10,14 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchController = TextEditingController();
+  bool isLoading = false;
+  QuerySnapshot? searchSnapshot;
+  bool hasUserSearched = false;
+  String userName = "";
+  bool isJoined = false;
+  User? user;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
