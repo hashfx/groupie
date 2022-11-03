fe604a5954db4b95bcf5dfb9c42484e1276312d0
Cirrus CI <support@cirruslabs.org>
2022-11-03 12:20:35 +0000

    noGroupWidget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -304,4 +304,33 @@ class _HomePageState extends State<HomePage> {
       },
     );
   }
+
+  noGroupWidget() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              popUpDialog(context);
+            },
+            child: Icon(
+              Icons.add_circle,
+              color: Colors.grey[700],
+              size: 75,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "You've not joined any groups, tap on the add icon to create a group or also search from top search button.",
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
 }
