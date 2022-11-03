080134d617a041ed7166e8fd14280efbb30951bf
Cirrus CI <support@cirruslabs.org>
2022-11-03 12:39:04 +0000

    getChatAndAdmin initState

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:groupie/service/database_service.dart';
 
 class ChatPage extends StatefulWidget {
   final String groupId;
@@ -21,7 +22,24 @@ class _ChatPageState extends State<ChatPage> {
   TextEditingController messageController = TextEditingController();
   String admin = "";
 
+  @override
+  void initState() {
+    getChatandAdmin();
+    super.initState();
+  }
 
+  getChatandAdmin() {
+    DatabaseService().getChats(widget.groupId).then((val) {
+      setState(() {
+        chats = val;
+      });
+    });
+    DatabaseService().getGroupAdmin(widget.groupId).then((val) {
+      setState(() {
+        admin = val;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
