f808a229d7a261f9bfe8a025064fb2f21b404cc9
Cirrus CI <support@cirruslabs.org>
2022-11-03 12:37:20 +0000

    chat page init

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  final String groupId;
+  final String groupName;
+  final String userName;
+  const ChatPage(
+      {Key? key,
+      required this.groupId,
+      required this.groupName,
+      required this.userName})
+      : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  Stream<QuerySnapshot>? chats;
+  TextEditingController messageController = TextEditingController();
+  String admin = "";
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
