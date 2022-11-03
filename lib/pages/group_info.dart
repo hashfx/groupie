747e6e90474a9f4f728a39f5926d8aaf8f4cf012
Cirrus CI <support@cirruslabs.org>
2022-11-03 13:07:35 +0000

    action for appBar

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:groupie/pages/home_page.dart';
 import 'package:groupie/service/database_service.dart';
+import 'package:groupie/widgets/widgets.dart';
 
 class GroupInfo extends StatefulWidget {
   final String groupId;
@@ -52,6 +54,47 @@ class _GroupInfoState extends State<GroupInfo> {
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text("Group Info"),
       ),
+      actions: [
+        IconButton(
+            onPressed: () {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Exit"),
+                      content: const Text("Are you sure you exit the group? "),
+                      actions: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.cancel,
+                            color: Colors.red,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            DatabaseService(
+                                    uid: FirebaseAuth.instance.currentUser!.uid)
+                                .toggleGroupJoin(widget.groupId,
+                                    getName(widget.adminName), widget.groupName)
+                                .whenComplete(() {
+                              nextScreenReplace(context, const HomePage());
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.done,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            icon: const Icon(Icons.exit_to_app))
+      ],
     );
   }
 }
