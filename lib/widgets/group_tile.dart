25a42ff7f34d9dbd9e3b5be399a50114db5866d0
Cirrus CI <support@cirruslabs.org>
2022-11-03 12:33:03 +0000

    group tile init

---
+++ lib/widgets/group_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroupTile extends StatefulWidget {
+  const GroupTile({super.key});
+
+  @override
+  State<GroupTile> createState() => _GroupTileState();
+}
+
+class _GroupTileState extends State<GroupTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
