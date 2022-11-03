a6e8aefa839853dc31d33dead2ae04cda91f7819
Cirrus CI <support@cirruslabs.org>
2022-11-03 13:09:11 +0000

    about group

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -95,6 +95,50 @@ class _GroupInfoState extends State<GroupInfo> {
             },
             icon: const Icon(Icons.exit_to_app))
       ],
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Theme.of(context).primaryColor.withOpacity(0.2)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      widget.groupName.substring(0, 1).toUpperCase(),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Group: ${widget.groupName}",
+                        style: const TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text("Admin: ${getName(widget.adminName)}")
+                    ],
+                  )
+                ],
+              ),
+            ),
+            memberList(),
+          ],
+        ),
+      ),
     );
   }
 }
