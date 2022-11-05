b38dc3396c27d3fcf108afdf5ad327136806efa1
Cirrus CI <support@cirruslabs.org>
2022-11-05 09:28:28 +0000

    import MessageTile

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:groupie/pages/group_info.dart';
 import 'package:groupie/service/database_service.dart';
+import 'package:groupie/widgets/message_tile.dart';
 import 'package:groupie/widgets/widgets.dart';
 
 class ChatPage extends StatefulWidget {
